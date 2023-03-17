d314f961ae6cb63ff330dac9125700c1072d6e9d
Eva Hayek <e.hayek0320221@arts.ac.uk>
2023-03-17 12:30:49 +0000

    Add files via upload

---
+++ cardMatchGame/src/ofApp.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Card.hpp"
+#include <vector>
+#include "ofxGui.h"
+
+using namespace std;
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+    void setupGame();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    
+        ~ofApp();
+        int numSetCards;
+        int cardWidth;
+        int cardHeight;
+        vector<Card *> cards;
+        vector<Card *> cardsToTest;
+        
+        int mouseX;
+        int mouseY;
+        
+        int buffer; // for buffer between cards
+    
+        int tries;
+    
+        ofVec2f findFreePosition();
+        bool checkIfGameOver();
+        void testIfCardsMatch();
+    
+    // screens in the game 
+    void startScreen();
+    void gameScreen();
+    void gameOverScreen();
+    
+    //bool startGame;
+    string screen;
+    bool difficultyChosen;
+    bool replay;
+    bool addWrongChoiceText;
+    bool win; 
+    
+    ofTrueTypeFont triesFont;
+    ofTrueTypeFont timerFont;
+    ofTrueTypeFont matchFont;
+    ofTrueTypeFont gameOverFont;
+    ofTrueTypeFont instructionsFont;
+    ofTrueTypeFont wrongChoiceFont;
+    
+    ofColor matchColor;
+    ofColor triesColor;
+    ofColor instructionsColor;
+    
+    string matchString;
+    bool drawMatchString;
+    
+    void updateNumTries();
+    void updateTimer();
+
+    int startGameTime;
+    int startMatchTime;
+    int timer;
+    int elapsed;
+    
+    ofImage backgroundCard;
+    string getRandomFruit(vector<string> &fruits);
+    		
+};
